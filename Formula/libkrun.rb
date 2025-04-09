c7a01d726032219586456b38292feda0e306b7ab
Sergio Lopez <slp@redhat.com>
2025-04-09 12:50:02 +0200

    Update libkrun to 1.11.2, enable BLK and NET

--- Formula/libkrun.rb
+++ Formula/libkrun.rb
@@ -1,13 +1,13 @@
 class Libkrun < Formula
   desc "Dynamic library providing KVM-based process isolation capabilities"
   homepage "https://github.com/containers/libkrun"
-  url "https://github.com/containers/libkrun/releases/download/v1.5.1/v1.5.1-with_macos_prebuilts.tar.gz"
-  sha256 "f213db9d9b436e255dc01aedc6de2767f400d8e16dba2d95b1095aa0e996c9ba"
+  url "https://github.com/containers/libkrun/releases/download/v1.11.2/libkrun-1.11.2-prebuilt-aarch64.tar.gz"
+  sha256 "c73c254cc360ffec95d704bd7bdf240e443a3ab69ee390cad0ef0d190378deee"
   license "Apache-2.0"
 
   bottle do
     root_url "https://raw.githubusercontent.com/slp/homebrew-krun/master/bottles"
-    sha256 cellar: :any, arm64_ventura: "16e011e8d435e1b400ef1db790094acf47986e60e7f29a9aff39e6de94f43929"
+    sha256 cellar: :any, arm64_sequoia: "2eedc8025c4253c29e60be3c21637720f6bb9e2789667880bc012c437a920d93"
   end
 
   depends_on "rust" => :build
@@ -17,7 +17,7 @@ class Libkrun < Formula
   depends_on "libkrunfw"
 
   def install
-    system "make"
+    system "make", "BLK=1", "NET=1"
     system "make", "PREFIX=#{prefix}", "install"
   end
 

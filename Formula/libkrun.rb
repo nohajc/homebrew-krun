28c6d6340bc8fc50ed491705a7467129312a8fac
Sergio Lopez <slp@redhat.com>
2025-07-31 10:05:50 +0200

    Update libkrun to 1.14.0

--- Formula/libkrun.rb
+++ Formula/libkrun.rb
@@ -1,13 +1,14 @@
 class Libkrun < Formula
   desc "Dynamic library providing KVM-based process isolation capabilities"
   homepage "https://github.com/containers/libkrun"
-  url "https://github.com/containers/libkrun/releases/download/v1.11.2/libkrun-1.11.2-prebuilt-aarch64.tar.gz"
-  sha256 "c73c254cc360ffec95d704bd7bdf240e443a3ab69ee390cad0ef0d190378deee"
+  version "1.14.0"
+  url "https://github.com/containers/libkrun/releases/download/v1.14.0/libkrun-1.14.0-prebuilt-aarch64.tar.gz"
+  sha256 "c84c72701950d11e3183b3bb58d79c09716d2ced628158d6f0b62a2c6bd1e8de"
   license "Apache-2.0"
 
   bottle do
     root_url "https://raw.githubusercontent.com/slp/homebrew-krun/master/bottles"
-    sha256 cellar: :any, arm64_sequoia: "2eedc8025c4253c29e60be3c21637720f6bb9e2789667880bc012c437a920d93"
+    sha256 cellar: :any, arm64_sequoia: "3ab5da8c23c627e157424f178c02324e232a8b4f783949ba42ab60e90da2a236"
   end
 
   depends_on "rust" => :build

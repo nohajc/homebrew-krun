2d25298707dd9221d404a05025a7da26ab842ef7
Sergio Lopez <slp@redhat.com>
2025-07-31 09:52:01 +0200

    Update libkrunfw to 4.10.0

--- Formula/libkrunfw.rb
+++ Formula/libkrunfw.rb
@@ -1,14 +1,14 @@
 class Libkrunfw < Formula
   desc "Dynamic library bundling a Linux kernel in a convenient storage format"
   homepage "https://github.com/containers/libkrunfw"
-  version "4.9.0"
-  url "https://github.com/containers/libkrunfw/releases/download/v4.9.0/libkrunfw-4.9.0-prebuilt-aarch64.tar.gz"
-  sha256 "42f59b7ea3a74240fc326fafa71b8687347ba68397762902ca2fed4d2761315c"
+  version "4.10.0"
+  url "https://github.com/containers/libkrunfw/releases/download/v4.10.0/libkrunfw-4.10.0-prebuilt-aarch64.tar.gz"
+  sha256 "6732e0424ce90fa246a4a75bb5f3357a883546dbca095fee07a7d587e82d94b0"
   license all_of: ["GPL-2.0-only", "LGPL-2.1-only"]
 
   bottle do
     root_url "https://raw.githubusercontent.com/slp/homebrew-krun/master/bottles"
-    sha256 cellar: :any, arm64_sequoia: "6b1f07978422e35c42d9df873e81f22a78cad63e5bb679f8b53463f54244ed35"
+    sha256 cellar: :any, arm64_sequoia: "2ec70626ad2cb60718e74ef6dcc9f4cc4932481b8bda4a84b8dbc37b2dd08d49"
   end
 
   # libkrun, our only consumer, only supports Hypervisor.framework on arm64

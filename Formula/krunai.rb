573c4a5e0f05021e16f1f15dd061d381354c9ed2
Sergio Lopez <slp@redhat.com>
2026-02-26 17:08:20 +0100

    krunai: update to version 0.2.1

--- Formula/krunai.rb
+++ Formula/krunai.rb
@@ -1,14 +1,14 @@
 class Krunai < Formula
   desc "An easy to use, fast and powerful tool for running AI agents inside microVM sandboxes."
   homepage "https://github.com/slp/krunai"
-  url "https://github.com/slp/krunai/archive/refs/tags/v0.2.0.tar.gz"
-  sha256 "9ce8419cbc6b0e7d9f893cefe8763eeeb62fcff2aac2250123fde9797f842939"
+  url "https://github.com/slp/krunai/archive/refs/tags/v0.2.1.tar.gz"
+  sha256 "39eb3da006c024b0724874fc69390d91c4a976525956ea59e57c3b643c0231ff"
   license "Apache-2.0"
 
   bottle do
     root_url "https://raw.githubusercontent.com/slp/homebrew-krun/master/bottles"
-    sha256 cellar: :any, arm64_tahoe: "dac8689d0a99970b8bce978056e98fbc8570326576ee0867bf7f660e14b9fa96"
-    sha256 cellar: :any, arm64_sequoia: "ac48a6f69f00794a942151bcff1782a7d0a11fae4cfed8b2d4fd509463a9a1c2"
+    sha256 cellar: :any, arm64_tahoe: "591952548fec76ccb953a9d223681b84c2812f7beb1edecb5b222f7934083778"
+    sha256 cellar: :any, arm64_sequoia: "d99a396390e2e64c2872fa9bfeac5f566628c02d9b6857d53838abd981cbaa6a"
   end
 
   depends_on "rust" => :build
